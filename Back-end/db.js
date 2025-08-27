8677ab29140bae7c12e911ba76fb21783322b867
0Pedrinn0 <pedrojonathadg@gmail.com>
2025-08-27 19:17:37 -0300

    Back-end: Adicionado

---
+++ Back-end/db.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mysql2 = require('mysql2')
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(mysql2());
+
+const express = require('');
+const cors = require('')
+const mysql2 = require('');
+
+app.use('', );
+app.use('', );
+app.use('', );
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:3000`);
+});
